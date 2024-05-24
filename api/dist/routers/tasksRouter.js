2a9384c0770ee9eec0fa8139936478f37001bd53
aks_an <aks_an07@mail.ru>
2024-05-24 21:55:48 +0700

    Изменились пути

--- api/dist/routers/tasksRouter.js
+++ api/dist/routers/tasksRouter.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const tasksController_1 = require("../controllers/tasksController");
 const router = (0, express_1.Router)();
-router.post("/add", tasksController_1.add);
-router.get("/receiving", tasksController_1.receiving);
+router.post("/", tasksController_1.add);
+router.get("/", tasksController_1.receiving);
 exports.default = router;
